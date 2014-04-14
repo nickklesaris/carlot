97975dfee42bd6359c37ccce8f8a9faff66f110e
Nick Klesaris <nickklesaris@gmail.com>
2014-04-14 09:42:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carlot::Application.config.secret_key_base = '01ee1cd8222c4e9729c692862a7227c7da1cbe0da8f7ea3cd7e8570575ad54db3bdb8c3cb7e49bdec86e95892b45f3e280cc562781c104ab3d1b762128b13c02'
